2dd245231145718024c6521f0b0d2232988cd17f
CH Usman <chusman2229@gmail.com>
2024-01-10 17:15:29 +0500

    Add project files.

---
+++ sportsCenterXam/sportsCenterXam/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using sportCenter.Enums;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sportCenter.Models
+{
+    public class User
+    {
+
+        [PrimaryKey]
+        public Guid UserCode { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+        public DateTime DateOfBirth { get; set; }
+        public bool IsMember { get; set; }
+        public UserGender Gender { get; set; }
+    }
+}
---
+++ sportsCenterXam/sportsCenterXam/Repositories/UserRepository/UserService.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.VisualBasic;
+using sportCenter.ApplicationConstants;
+using sportCenter.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sportCenter.Repositories.UserRepository
+{
+    public class UserService
+    {
+        //intialize the database
+        private SQLiteConnection Database;
+        public async Task Init()
+        {
+            if (Database != null)
+                return;
+
+            Database = new SQLiteConnection(DatabaseConstants.DatabasePath, DatabaseConstants.Flags);
+            var result = Database.CreateTable<User>();
+        }
+        //GetUsersAsync
+        public async Task<List<User>> GetUsersAsync()
+        {
+            await Init();
+            var users = Database.Table<User>().ToList();
+            return users;
+        }
+        //GetUserAsync
+        public async Task<User> GetUserAsync(Guid userCode)
+        {
+            await Init();
+            return Database.Table<User>().Where(u => u.UserCode == userCode).FirstOrDefault();
+        }
+        //SaveUserAsync
+        public async Task SaveUserAsync(User user)
+        {
+            await Init();
+            if (user.UserCode == Guid.Empty)
+            {
+                user.UserCode = Guid.NewGuid();
+                Database.Insert(user);
+                Database.Commit();
+            }
+            else
+            {
+                Database.Update(user);
+                Database.Commit();
+            }
+        }
+        //DeleteUserAsync
+        public async Task DeleteUserAsync(User user)
+        {
+            await Init();
+            Database.Delete(user);
+            Database.Commit();
+
+        }
+        //updateUserAsync
+        public async Task UpdateUserAsync(User user)
+        {
+            await Init();
+            Database.Update(user);
+            Database.Commit();
+
+        }
+        //GetUsersByGenderAsync
+        public async Task<List<User>> GetUsersByGender(string gender)
+        {
+            await Init();
+            return Database.Table<User>().Where(x => x.Gender.ToString() == gender).ToList();
+        }
+        //GetUsersByMemberAsync
+        public async Task<List<User>> GetUsersByMembership(bool isMember)
+        {
+            await Init();
+            var set = Database.Table<User>();
+            set = isMember ? set.Where(x => x.IsMember) : set.Where(x => !x.IsMember);
+            return set.ToList();
+        }
+
+        ~UserService()
+        {
+            Database?.Close();
+        }
+    }
+}
