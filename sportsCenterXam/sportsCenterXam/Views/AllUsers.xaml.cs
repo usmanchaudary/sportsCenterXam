0ecbc2a96b45e035ade7b4de1d18b5d4c9e041c5
CH Usman <chusman2229@gmail.com>
2024-01-11 15:28:37 +0500

    users fetch service to show in a list view added

--- sportsCenterXam/sportsCenterXam/Views/AllUsers.xaml.cs
+++ sportsCenterXam/sportsCenterXam/Views/AllUsers.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using sportCenter.Models;
+using sportCenter.Repositories.UserRepository;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +15,27 @@ namespace sportsCenterXam.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AllUsers : ContentPage
     {
+        //create an observable collection of users
+        ObservableCollection<User> users = new ObservableCollection<User>();
         public AllUsers()
         {
             InitializeComponent();
+            GetUsers().ContinueWith(t =>
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    //add users to the observable collection
+                    foreach (var user in t.Result)
+                    {
+                        users.Add(user);
+                    }
+                });
+            });
+        }
+        private async Task<List<User>> GetUsers()
+        {
+            var userService = new UserService();
+            return await userService.GetUsersAsync();
         }
     }
 }
